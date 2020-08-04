e71af6578fca9afacc0babb56f924e69ad3b369e
K3V1N <kevin@ffeu.de>
2020-08-04 13:18:46 +0200

    some changes

--- site.mk
+++ site.mk
@@ -54,7 +54,7 @@ GLUON_SITE_PACKAGES := \
 #			opkg compare-versions "$1" '>>' "$2"
 #		to decide if a version is newer or not.
 
-DEFAULT_GLUON_RELEASE := fastd-2018.2.3-2.0.3+exp$(shell date '+%Y%m%d')
+DEFAULT_GLUON_RELEASE := fastd-2020.1.3-3.0.0+exp$(shell date '+%Y%m%d')
 
 # Variables set with ?= can be overwritten from the command line
 
